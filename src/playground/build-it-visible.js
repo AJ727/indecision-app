3cca7dba234be574e6fb3ff3a05b191872614b58
Austin <austin1911remo@gmail.com>
2018-01-16 17:13:48 -0500

    Build It: Visibility Toggle S4L35
    Built visibility toggle using state

--- src/playground/build-it-visible.js
+++ src/playground/build-it-visible.js
@@ -1,21 +1,39 @@
 class VisibilityToggle extends React.Component {
     constructor(props){
         super(props);
+        this.handleToggleVisibility = this.handleToggleVisibility.bind(this);
+        this.state = {
+            visibility : false
+        }
     }
     handleToggleVisibility() {
-
+        this.setState((prevState) => {
+            return {
+                visibility : !prevState.visibility
+            } 
+        })
     }
     render(){
         return(
-            <p></p>
+            <div>
+            <h1>visibility toggle</h1>
+            <button onClick={this.handleToggleVisibility}>
+                {this.state.visibility ? 'Hide details' : 'Show details'}
+            </button>
+            {this.state.visibility && (
+                <div>
+                    <p>Hiya</p>
+                </div>
+            )}
+        </div>   
         )
     }
 }
 
+ReactDOM.render(<VisibilityToggle />, document.getElementById('app'));
 
 
-
-
+/*
 let visibility = false; 
 
 const toggleVis = () => {
@@ -42,3 +60,4 @@ const reRender = () => {
 }
 
 reRender();
+*/
