7e36ae77d0300d533eb821e1fdc0a714318996f1
Austin <austin1911remo@gmail.com>
2018-05-18 11:51:06 -0400

    Webpack Dev Server S6L57

--- webpack.config.js
+++ webpack.config.js
@@ -14,5 +14,8 @@ module.exports = {
             exclude: /node_modules/
         }]
     },
-    devtool: 'cheap-module-eval-source-map'
+    devtool: 'cheap-module-eval-source-map',
+    devServer: {
+        contentBase: path.join(__dirname, 'public')
+    }
 };
