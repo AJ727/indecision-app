0649bdae2aa84f1baa1e2d37ae10b25013e34211
Austin <austin1911remo@gmail.com>
2018-05-21 22:39:52 -0400

    Passing Children to Component S7L60

--- src/app.js
+++ src/app.js
@@ -2,4 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import IndecisionApp from './components/IndecisionApp';
 
+const Layout = (props) => {
+    return(
+        <div>
+            <p>header</p>
+            {props.children}
+            <p>footer</p>
+        </div>
+    );
+}
+
 ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
