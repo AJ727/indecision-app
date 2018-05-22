faec157d33751cee746cad0810280802789c4851
Austin <austin1911remo@gmail.com>
2018-05-21 22:31:23 -0400

    ES6 Class Properties S6L58
    Implemented class properties, allowing for the deletion of the constructors

--- src/app.js
+++ src/app.js
@@ -3,4 +3,3 @@ import ReactDOM from 'react-dom';
 import IndecisionApp from './components/IndecisionApp';
 
 ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
-
--- src/components/IndecisionApp.js
+++ src/components/IndecisionApp.js
@@ -5,53 +5,25 @@ import Header from './Header';
 import Action from './Action';
 
 export default class IndecisionApp extends React.Component {
-    constructor(props){
-        super(props);
-        this.handleDeleteOptions = this.handleDeleteOptions.bind(this);
-        this.handlePick = this.handlePick.bind(this);
-        this.handleAddOption = this.handleAddOption.bind(this);
-        this.handleDeleteOption = this.handleDeleteOption.bind(this);
-        this.state = {
-            options : []
-        };
-    }
-    componentDidMount() {
-        try {
-            const json = localStorage.getItem('options');
-            const options = JSON.parse(json);
-            
-            if(options) {
-                this.setState(() => ({ options }));
-            }
-        } catch(e) {
-            // Do nothing
-        }
-    }
-    componentDidUpdate(prevProps, prevState) {
-        if(prevState.options.length !== this.state.options.length) {
-            const json = JSON.stringify(this.state.options);
-            localStorage.setItem('options', json);
-        }
-    }
-    componentWillUnmount() {
-        console.log('unmount!')
-    }
-    handleDeleteOptions() {
+    state = {
+        options: []
+    };
+    handleDeleteOptions = () => {
         this.setState(() => ({ options: [] }))
-    }
+    };
     handleDeleteOption(optionToRemove) {
         this.setState((prevState) => ({
             options: prevState.options.filter((option) => 
                 optionToRemove !== option
             )
         }))
-    }
-    handlePick() {
+    };
+    handlePick = () => {
         const randNum = Math.floor(Math.random() * this.state.options.length);
         const option = this.state.options[randNum];
         alert(option);
-    }
-    handleAddOption(option) {
+    };
+    handleAddOption = (option) => {
         if(!option){
             return 'Enter valid value to add item';
         }
@@ -63,7 +35,28 @@ export default class IndecisionApp extends React.Component {
         this.setState((prevState) => ({
                 options: prevState.options.concat(option)
         }))
-    }
+    };
+    componentDidMount() {
+        try {
+            const json = localStorage.getItem('options');
+            const options = JSON.parse(json);
+            
+            if(options) {
+                this.setState(() => ({ options }));
+            }
+        } catch(e) {
+            // Do nothing
+        }
+    };
+    componentDidUpdate(prevProps, prevState) {
+        if(prevState.options.length !== this.state.options.length) {
+            const json = JSON.stringify(this.state.options);
+            localStorage.setItem('options', json);
+        }
+    };
+    componentWillUnmount() {
+        console.log('unmount!')
+    };
     render() {
         const subtitle = "Put your life in the hands of a computer";
         return (
@@ -83,7 +76,7 @@ export default class IndecisionApp extends React.Component {
                 />
             </div>
         )
-    }
+    };
 }
 
 
