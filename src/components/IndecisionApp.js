49e9fe184bd7c4ead39345192fd1e0bddfa05f64
Austin <austin1911remo@gmail.com>
2018-05-22 20:42:16 -0400

    Theming With Variables S8L67
    _settings.scss is created to hold commonly reused colors and spacings

--- src/components/IndecisionApp.js
+++ src/components/IndecisionApp.js
@@ -15,7 +15,7 @@ export default class IndecisionApp extends React.Component {
     };
     handleDeleteOption(optionToRemove) {
         this.setState((prevState) => ({
-            options: prevState.options.filter((option) => 
+            options: prevState.options.filter((option) =>
                 optionToRemove !== option
             )
         }));
@@ -33,32 +33,32 @@ export default class IndecisionApp extends React.Component {
         }));
     };
     handleAddOption = (option) => {
-        if(!option){
+        if (!option) {
             return 'Enter valid value to add item';
         }
 
-        else if(this.state.options.indexOf(option) > -1){
+        else if (this.state.options.indexOf(option) > -1) {
             return 'This option already exists';
         }
 
         this.setState((prevState) => ({
-                options: prevState.options.concat(option)
+            options: prevState.options.concat(option)
         }))
     };
     componentDidMount() {
         try {
             const json = localStorage.getItem('options');
             const options = JSON.parse(json);
-            
-            if(options) {
+
+            if (options) {
                 this.setState(() => ({ options }));
             }
-        } catch(e) {
+        } catch (e) {
             // Do nothing
         }
     };
     componentDidUpdate(prevProps, prevState) {
-        if(prevState.options.length !== this.state.options.length) {
+        if (prevState.options.length !== this.state.options.length) {
             const json = JSON.stringify(this.state.options);
             localStorage.setItem('options', json);
         }
@@ -71,22 +71,24 @@ export default class IndecisionApp extends React.Component {
         return (
             <div>
                 <Header subtitle={subtitle} />
-                <Action 
-                    hasOptions={this.state.options.length > 0}
-                    handlePick={this.handlePick}
-                />
-                <Options 
-                    options={this.state.options}
-                    handleDeleteOptions = {this.handleDeleteOptions}
-                    handleDeleteOption = {this.handleDeleteOption}
-                />
-                <AddOption 
-                    handleAddOption = {this.handleAddOption}
-                />
-                <OptionModal 
-                    selectedOption = {this.state.selectedOption}
-                    handleCloseWindow = {this.handleCloseWindow}
-                />
+                <div className="container">
+                    <Action
+                        hasOptions={this.state.options.length > 0}
+                        handlePick={this.handlePick}
+                    />
+                    <Options
+                        options={this.state.options}
+                        handleDeleteOptions={this.handleDeleteOptions}
+                        handleDeleteOption={this.handleDeleteOption}
+                    />
+                    <AddOption
+                        handleAddOption={this.handleAddOption}
+                    />
+                    <OptionModal
+                        selectedOption={this.state.selectedOption}
+                        handleCloseWindow={this.handleCloseWindow}
+                    />
+                </div>
             </div>
         )
     };
