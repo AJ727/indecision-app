5a9076a783e544f6f6c3244a211de0b359b87a8b
Austin <austin1911remo@gmail.com>
2018-05-27 18:35:13 -0400

    Styling the Options List S8L69
    Added _widget.scss and a couple more items to style the widget

--- src/components/IndecisionApp.js
+++ src/components/IndecisionApp.js
@@ -76,18 +76,20 @@ export default class IndecisionApp extends React.Component {
                         hasOptions={this.state.options.length > 0}
                         handlePick={this.handlePick}
                     />
-                    <Options
-                        options={this.state.options}
-                        handleDeleteOptions={this.handleDeleteOptions}
-                        handleDeleteOption={this.handleDeleteOption}
-                    />
-                    <AddOption
-                        handleAddOption={this.handleAddOption}
-                    />
-                    <OptionModal
-                        selectedOption={this.state.selectedOption}
-                        handleCloseWindow={this.handleCloseWindow}
-                    />
+                    <div className="widget">
+                        <Options
+                            options={this.state.options}
+                            handleDeleteOptions={this.handleDeleteOptions}
+                            handleDeleteOption={this.handleDeleteOption}
+                        />
+                        <AddOption
+                            handleAddOption={this.handleAddOption}
+                        />
+                        <OptionModal
+                            selectedOption={this.state.selectedOption}
+                            handleCloseWindow={this.handleCloseWindow}
+                        />
+                    </div>
                 </div>
             </div>
         )
